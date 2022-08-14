127c287c4c969da54fa02266e989c27ad68a1995
William Leung <gwilliamleung@gmail.com>
2022-08-13 21:31:08 -0400

    project finished

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 document.getElementById("btn").addEventListener("click",function(){
     const colorPicked = document.getElementById("color").value
     const schemePicked = document.getElementById("scheme").value
-    console.log(colorPicked,schemePicked)
-    fetch(`https://www.thecolorapi.com/scheme?hex=${colorPicked.substring(1)}`)
+    fetch(`https://www.thecolorapi.com/scheme?hex=${colorPicked.substring(1)}&mode=${schemePicked.toLowerCase()}`)
         .then(res => res.json())
         .then(data => {
+            console.log(data.colors[1].name.value)
             document.getElementById("colorBody").innerHTML = ''
+            document.getElementById("textBody").innerHTML = ''
             for (var i = 0 in data.colors){
                 document.getElementById("colorBody").innerHTML += `
-                <h1>${data.colors[i].hex.value}</h1>`
+                <div class="colorRow" id="color${i}"></div>`
+                document.getElementById("textBody").innerHTML += `
+                <div class="textRow">${data.colors[i].hex.value}</div>`
+                document.getElementById(`color${i}`).style.backgroundColor = `${data.colors[i].hex.value}`
             }
         })
 })
